9aa249c5c5b0e6000b30cc1d38b57547e9dd2a02
Luiz Francisco <luizcostaneto2505@gmail.com>
2024-05-23 02:57:16 -0300

    desafio conta bancaria

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,10 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
-        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        System.out.println("Digite o número: ");
+        int numero = scanner.nextInt();
 
+        System.out.println("Digite a agência: ");
+        String agencia = scanner.next();
 
+        System.out.println("Digite seu primeiro nome: ");
+        String primeiroNome = scanner.next();
 
+        System.out.println("Digite seu sobrenome:");
+        String sobreNome = scanner.next();
+        System.out.println("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println(nomeCompleto(primeiroNome, sobreNome));
+        System.out.println("Agência: " + agencia);
+        System.out.println("Conta: " + numero);
+        System.out.println("Seu saldo: " + saldo + " já está disponível para saque.");
     }
+
+        public static String nomeCompleto(String primeiroNome, String sobreNome){
+            return "Olá, " + primeiroNome.concat(" ").concat(sobreNome) + "! Obrigado por criar uma conta em nosso banco.";
+        }
 }
